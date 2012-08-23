514b26c12b81dc6207dc826e10ffac1fe3bb462d
Ezcaflowne <ezcaflowne@fullsail.edu>
2012-08-23 02:44:03 -0700

    Added Project 4 Files to gh-pages

---
+++ ayotte_willson_project4/js/json.js
@@ -0,0 +1,41 @@
+// Name: Willson Ayotte
+// Subject: JSON file used to keep data in local storage for testing.
+// Date: Aug. 19, 2012
+// Decription: This is for testing purposes only and is not to be used when the project is in
+// final stage, and ready for production.
+
+var json = {
+	"apartment1": {
+		"aptType" 		: ["Type:", "Vacant Available"],
+		"aptNum"  		: ["Apartment Number:", "1079"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "8/19/2012"],
+		"rdyDate" 		: ["Ready:", "8/29/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "20"],
+		"comments"		: ["Additional Comments:", "Needs new carpet and vinyl installed."]
+	},
+	"apartment2": {
+		"aptType" 		: ["Type:", "Vacant Rented"],
+		"aptNum"  		: ["Apartment Number:", "2121"],
+		"aptSize" 		: ["Apartment Size:", "2x2"],
+		"vacDate" 		: ["Vacate:", "8/9/2012"],
+		"rdyDate" 		: ["Ready:", "8/19/2012"],
+		"isPower" 		: ["Power?", "Yes"],
+		"isWhiteLock" 	: ["Whitelock?", "Yes"],
+		"condition" 	: ["Condition:", "100"],
+		"comments"		: ["Additional Comments:", "Countertops have been resurfaced."]
+	},
+	"apartment3": {
+		"aptType" 		: ["Type:", "Notice Available"],
+		"aptNum"  		: ["Apartment Number:", "1003"],
+		"aptSize" 		: ["Apartment Size:", "1x1"],
+		"vacDate" 		: ["Vacate:", "8/27/2012"],
+		"rdyDate" 		: ["Ready:", ""],
+		"isPower" 		: ["Power?", ""],
+		"isWhiteLock" 	: ["Whitelock?", ""],
+		"condition" 	: ["Condition:", "50"],
+		"comments"		: ["Additional Comments:", "Resident would like apartment walked prior to move-out date."]
+	}
+};
